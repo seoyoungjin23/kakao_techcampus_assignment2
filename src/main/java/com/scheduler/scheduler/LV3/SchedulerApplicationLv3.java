f84e1c0b4e306872e37907b0940f49b7ed259052
seoyoungjin23 <ascd050@gmail.com>
2025-05-25 21:29:29 +0900

    fix: Lv3 어플리케이션을 실행하도록 수정

--- src/main/java/com/scheduler/scheduler/LV3/SchedulerApplicationLv3.java
+++ src/main/java/com/scheduler/scheduler/LV3/SchedulerApplicationLv3.java
@@ -1,6 +1,5 @@
 package com.scheduler.scheduler.LV3;
 
-import com.scheduler.scheduler.LV2.SchedulerApplicationLv2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SchedulerApplicationLv3 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SchedulerApplicationLv2.class, args);
+		SpringApplication.run(SchedulerApplicationLv3.class, args);
 	}
 
 }
